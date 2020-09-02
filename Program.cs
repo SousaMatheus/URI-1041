848ad55723a41587cc491ba85b5b2cb1511ee4b3
Matheus Sousa <ma.sousa.soares@gmail.com>
2020-09-01 22:34:28 -0300

    alterado nome das variaveis valor1 e valor2 para x e y respectivamente

--- Program.cs
+++ Program.cs
@@ -1,41 +1,39 @@
 ﻿using System;
 using System.Globalization;
 
-namespace URI_1041
-{
-    class Program
+namespace URI_1041{
+        class Program
     {
         static void Main(string[] args)
         {
-            double valor1, valor2;
+            double x,y;
 
             string []vet = Console.ReadLine().Split(' ');
-            valor1 = double.Parse(vet[0],CultureInfo.InvariantCulture);
-            valor2 = double.Parse(vet[1],CultureInfo.InvariantCulture);
+            x = double.Parse(vet[0],CultureInfo.InvariantCulture);
+            y = double.Parse(vet[1],CultureInfo.InvariantCulture);
 
-            if (valor1 == 0.0 && valor2 == 0.0){
+            if (x == 0.0 && y == 0.0){
                 Console.WriteLine("Origem");
             }
-            else if (valor1 == 0.0 && valor2 > 0.0 || valor2 < 0.0){
+            else if (x == 0.0){
                 Console.WriteLine("Eixo X");                
             }
-            else if (valor1 >0.0 || valor1 <0.0 && valor2 == 0.0){
+            else if (y == 0.0){
                 Console.WriteLine("Eixo Y");
             }
 
-            else if (valor1 > 0.0 && valor2 > 0.0){
+            else if (x > 0.0 && y > 0.0){
                 Console.WriteLine("Q1");
             }
-            else if (valor1 >0.0 && valor2 <0.0){
+            else if (x >0.0 && y <0.0){
                 Console.WriteLine("Q4");
             }
-            else if ( valor1 < 0.0 && valor2 > 0.0){
+            else if (x < 0.0 && y > 0.0){
                 Console.WriteLine("Q2");
             }
             else {
                 Console.WriteLine("Q3");
-            }
-            
+            }            
 
         }
     }
